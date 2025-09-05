8a9d4d67efe5f97ea78a1933b9742573b74ddc59
Yaroslav Bazko <ybazko08@gmail.com>
2025-09-05 17:21:53 +0300

    Review, Comment

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, GenreViewSet, TitleViewSet
+from .views import (CategoryViewSet, GenreViewSet,
+                    TitleViewSet, ReviewViewSet, CommentViewSet)
 
 app_name = 'api'
 
@@ -9,7 +10,14 @@ router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='categories')
 router.register(r'genres', GenreViewSet, basename='genres')
 router.register(r'titles', TitleViewSet, basename='titles')
+reviews_router.register(r'reviews', ReviewViewSet, basename='reviews')
 
+comments_router = DefaultRouter()
+comments_router.register(r'comments', CommentViewSet, basename='comments')
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('v1/titles/<int:title_id>/', include(reviews_router.urls)),
+    path(
+        'v1/titles/<int:title_id>/reviews/<int:review_id>/',
+        include(comments_router.urls)),
 ]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets, mixins
 from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from reviews.models import Category, Genre, Title
 from .serializers import (CategorySerializer, GenreSerializer,
-                          TitleSerializer, TitleCreateSerializer)
+                          TitleSerializer, TitleCreateSerializer,
+                          ReviewSerializer, CommentSerializer)
+from .permissions import IsAuthorOrReadOnly
+from reviews.models import Review, Comment, Title
 
 
 class CategoryViewSet(mixins.ListModelMixin,
@@ -35,3 +39,31 @@ class TitleViewSet(viewsets.ModelViewSet):
         if self.action in ['create', 'update', 'partial_update']:
             return TitleCreateSerializer
         return TitleSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        return Review.objects.filter(title__id=title_id)
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = Title.objects.get(id=title_id)
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        review_id = self.kwargs.get('review_id')
+        return Comment.objects.filter(review__id=review_id)
+
+    def perform_create(self, serializer):
+        review_id = self.kwargs.get('review_id')
+        review = Review.objects.get(id=review_id)
+        serializer.save(author=self.request.user, review=review)
